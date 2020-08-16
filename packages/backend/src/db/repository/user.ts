297aedee6a3365698765daccce3108b894dea166
DecentM <decentm@decentm.com>
2020-08-16 05:07:53 +0300

    refactor: move login logic to user repository

--- packages/backend/src/db/repository/user.ts
+++ packages/backend/src/db/repository/user.ts
@@ -1,8 +1,67 @@
 import { Repository, EntityRepository } from 'typeorm'
+import bcrypt from 'bcrypt'
+
+import { log as logger } from '@/lib/log'
 import { User } from '../entity/user'
+
+const log = logger.child({
+  component: 'user-repository',
+})
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  /**
+   * Logs a user in. Finds by email address and checks eligibility for login.
+   *
+   * @param email Email address of the user trying to log in
+   * @param password Password of the user, this will be hashed and checked against the stored hash
+   * @returns Promise<User>
+   * @throws Error
+   */
+  public async login (email: string, password: string): Promise<User> {
+    const user = await this.findOne({
+      email: email.toLowerCase(),
+    })
+
+    const authFailedError = new Error('Authentication failed')
+
+    if (!user) {
+      log.info({ email }, 'auth failed because no user')
+
+      throw authFailedError
+    }
+
+    if (!user.provisioned) {
+      log.error({
+        userId: user.id,
+        userDisplay: user.display,
+      }, 'unprovisioned user tried to log in')
+
+      throw authFailedError
+    }
+
+    if (user.disabled) {
+      log.error({
+        userId: user.id,
+        userDisplay: user.display,
+      }, 'disabled user tried to log in')
+
+      throw authFailedError
+    }
+
+    const result = await bcrypt.compare(password, user.password)
+
+    if (!result) {
+      log.warn({
+        userId: user.id,
+      }, 'auth failed because password')
+
+      throw authFailedError
+    }
+
+    return user
+  }
+
   public async register (email: string, password: string, display: string): Promise<User> {
     const duplicate = await this.findOne({
       where: {
--- packages/backend/src/graphql/passport/strategies/graphql-local.ts
+++ packages/backend/src/graphql/passport/strategies/graphql-local.ts
@@ -1,58 +1,20 @@
 import { GraphQLLocalStrategy } from 'graphql-passport'
 import { Connection } from 'typeorm'
 import { Request } from 'express'
-import bcrypt from 'bcrypt'
 
 import { Config } from '@/lib/config/type'
 import { User } from '~/db/entity'
-import { log as baseLog } from '@/lib/log'
-
-const log = baseLog.child({
-  component: 'passport-strategy-graphql-local',
-})
+import { UserRepository } from '~/db/repository/user'
 
 export const makeStrategy = (config: Config, connection: Connection): GraphQLLocalStrategy<User, Request> => new GraphQLLocalStrategy(
   async (email: string, password: string, done: (err: Error | null, result?: User) => void) => {
-    const user = await connection.getRepository(User)
-      .findOne({
-        email,
-      })
+    try {
+      const user = await connection.getCustomRepository(UserRepository)
+        .login(email, password)
 
-    const authFailedError = new Error('Authentication failed')
-
-    if (!user) {
-      log.info({ email }, 'auth failed because no user')
-
-      return done(authFailedError)
+      return done(null, user)
+    } catch (error) {
+      return done(error)
     }
-
-    if (!user.provisioned) {
-      log.error({
-        userId: user.id,
-        userDisplay: user.display,
-      }, 'unprovisioned user tried to log in')
-
-      return done(authFailedError, null)
-    }
-
-    if (user.disabled) {
-      log.error({
-        userId: user.id,
-        userDisplay: user.display,
-        userEmail: user.email,
-      }, 'disabled user tried to log in')
-
-      return done(authFailedError, null)
-    }
-
-    const result = await bcrypt.compare(password, user.password)
-
-    if (!result) {
-      log.warn({ email }, 'auth failed because password')
-
-      return done(authFailedError)
-    }
-
-    done(null, result ? user : null)
   },
 )
